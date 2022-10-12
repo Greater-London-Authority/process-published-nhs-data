10240e15e8d0778627749ccf539ee1fd1d2bce87
Ben Corr <Ben.Corr@london.gov.uk>
2022-10-12 18:00:35 +0100

    round output values to 1dp. include csv output files

--- R/5_create_output_files.R
+++ R/5_create_output_files.R
@@ -5,9 +5,12 @@ source("R/functions/aggregate_to_region.R")
 fpath <- list(
   processed_data = "data/processed/",
   gp_sya_res_month = "data/intermediate/gp_res_sya/",
-  output_gp_sya_lad = "data/processed/gp_sya_lad.rds",
-  output_gp_sya_rgn = "data/processed/gp_sya_rgn.rds",
-  output_gp_sya_itl = "data/processed/gp_sya_itl.rds",
+  lad_output_rds = "data/processed/gp_sya_lad.rds",
+  rgn_output_rds = "data/processed/gp_sya_rgn.rds",
+  itl_output_rds = "data/processed/gp_sya_itl.rds",
+  lad_output_csv = "data/processed/gp_sya_lad.csv",
+  rgn_output_csv = "data/processed/gp_sya_rgn.csv",
+  itl_output_csv = "data/processed/gp_sya_itl.csv",
   lookup_lad_rgn = "lookups/lookup_lad_rgn.rds",
   lookup_lad_itl = "lookups/lookup_lad_itl.rds"
 )
@@ -20,9 +23,11 @@ gp_sya_res_lad <- lapply(list.files(fpath$gp_sya_res_month, full.names = TRUE), 
          geography = "LAD21") %>%
   filter(grepl("E0", gss_code)) %>%
   add_persons() %>%
+  mutate(value = round(value, 1)) %>%
   arrange(gss_code, sex, extract_date, age)
 
-saveRDS(gp_sya_res_lad, fpath$output_gp_sya_lad)
+saveRDS(gp_sya_res_lad, fpath$lad_output_rds)
+write_csv(gp_sya_res_lad, fpath$lad_output_csv)
 
 # create aggregations for regions and subregions and save as separate files
 gp_sya_res_rgn <- aggregate_to_region(gp_sya_res_lad,
@@ -30,7 +35,8 @@ gp_sya_res_rgn <- aggregate_to_region(gp_sya_res_lad,
   mutate(geography = "RGN21") %>%
   arrange(gss_code, sex, extract_date, age)
 
-saveRDS(gp_sya_res_rgn, fpath$output_gp_sya_rgn)
+saveRDS(gp_sya_res_rgn, fpath$rgn_output_rds)
+write_csv(gp_sya_res_rgn, fpath$rgn_output_csv)
 
 
 gp_sya_res_itl <- aggregate_to_region(gp_sya_res_lad,
@@ -38,4 +44,5 @@ gp_sya_res_itl <- aggregate_to_region(gp_sya_res_lad,
   mutate(geography = "ITL221") %>%
   arrange(gss_code, sex, extract_date, age)
 
-saveRDS(gp_sya_res_itl, fpath$output_gp_sya_itl)
+saveRDS(gp_sya_res_itl, fpath$itl_output_rds)
+write_csv(gp_sya_res_itl, fpath$itl_output_csv)
