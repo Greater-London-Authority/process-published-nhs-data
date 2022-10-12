1e17320bad93cb142d610966fa505051eaf51274
Ben Corr <Ben.Corr@london.gov.uk>
2022-10-12 16:48:01 +0100

    add column specifying geography used to output files

--- R/5_create_output_files.R
+++ R/5_create_output_files.R
@@ -16,7 +16,8 @@ if(!dir.exists(fpath$processed_data)) dir.create(fpath$processed_data, recursive
 
 gp_sya_res_lad <- lapply(list.files(fpath$gp_sya_res_month, full.names = TRUE), readRDS) %>%
   bind_rows() %>%
-  mutate(measure = "gp_count") %>%
+  mutate(measure = "gp_count",
+         geography = "LAD21") %>%
   filter(grepl("E0", gss_code)) %>%
   add_persons() %>%
   arrange(gss_code, sex, extract_date, age)
@@ -26,12 +27,15 @@ saveRDS(gp_sya_res_lad, fpath$output_gp_sya_lad)
 # create aggregations for regions and subregions and save as separate files
 gp_sya_res_rgn <- aggregate_to_region(gp_sya_res_lad,
                                       readRDS(fpath$lookup_lad_rgn)) %>%
+  mutate(geography = "RGN21") %>%
   arrange(gss_code, sex, extract_date, age)
 
 saveRDS(gp_sya_res_rgn, fpath$output_gp_sya_rgn)
 
+
 gp_sya_res_itl <- aggregate_to_region(gp_sya_res_lad,
                                       readRDS(fpath$lookup_lad_itl)) %>%
+  mutate(geography = "ITL221") %>%
   arrange(gss_code, sex, extract_date, age)
 
 saveRDS(gp_sya_res_itl, fpath$output_gp_sya_itl)
