ade916b3ce7113d9fd1fa23ed77c325c8761fa96
Ben Corr <Ben.Corr@london.gov.uk>
2022-10-13 06:04:58 +0100

    change format of csv outputs to a cross-tab by age to reduce file sizes and make more accessible to users working in Excel

--- R/5_create_output_files.R
+++ R/5_create_output_files.R
@@ -27,22 +27,41 @@ gp_sya_res_lad <- lapply(list.files(fpath$gp_sya_res_month, full.names = TRUE), 
   arrange(gss_code, sex, extract_date, age)
 
 saveRDS(gp_sya_res_lad, fpath$lad_output_rds)
-write_csv(gp_sya_res_lad, fpath$lad_output_csv)
 
-# create aggregations for regions and subregions and save as separate files
+gp_sya_res_lad_wide <- gp_sya_res_lad %>%
+  pivot_wider(names_from = "age",
+              values_from = "value",
+              names_prefix = "age_")
+
+write_csv(gp_sya_res_lad_wide, fpath$lad_output_csv)
+
+# create aggregations for regions
 gp_sya_res_rgn <- aggregate_to_region(gp_sya_res_lad,
                                       readRDS(fpath$lookup_lad_rgn)) %>%
   mutate(geography = "RGN21") %>%
   arrange(gss_code, sex, extract_date, age)
 
 saveRDS(gp_sya_res_rgn, fpath$rgn_output_rds)
-write_csv(gp_sya_res_rgn, fpath$rgn_output_csv)
 
+gp_sya_res_rgn_wide <- gp_sya_res_rgn %>%
+  pivot_wider(names_from = "age",
+              values_from = "value",
+              names_prefix = "age_")
 
+write_csv(gp_sya_res_rgn_wide, fpath$rgn_output_csv)
+
+# create aggregatations for ITL2 subregions
 gp_sya_res_itl <- aggregate_to_region(gp_sya_res_lad,
                                       readRDS(fpath$lookup_lad_itl)) %>%
   mutate(geography = "ITL221") %>%
   arrange(gss_code, sex, extract_date, age)
 
 saveRDS(gp_sya_res_itl, fpath$itl_output_rds)
-write_csv(gp_sya_res_itl, fpath$itl_output_csv)
+
+
+gp_sya_res_itl_wide <- gp_sya_res_itl %>%
+  pivot_wider(names_from = "age",
+              values_from = "value",
+              names_prefix = "age_")
+
+write_csv(gp_sya_res_itl_wide, fpath$itl_output_csv)
