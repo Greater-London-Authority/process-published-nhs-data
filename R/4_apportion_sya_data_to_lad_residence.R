b4ca133af6c3a1aa9c310eaeb976e86eaa9b3ee9
Marta Lapsley <marta.lapsley@london.gov.uk>
2024-04-24 15:09:21 +0100

    finished only process new data

--- R/4_apportion_sya_data_to_lad_residence.R
+++ R/4_apportion_sya_data_to_lad_residence.R
@@ -20,9 +20,9 @@ gp_res_dates <- substr(list.files(fpath$gp_lad_month), 1, 7)
 last_res_date <- max(gp_res_dates)
 
 gp_sya_dates <- substr(list.files(fpath$gp_sya_month), 1, 7)
-processed_sya_lsoa_dates <- substr(list.files(fpath$sya_lad_month), 1, 7)
+processed_sya_lad_dates <- substr(list.files(fpath$sya_lad_month), 1, 7)
 new_gp_sya_dates <- gp_sya_dates[!gp_sya_dates %in% processed_sya_lsoa_dates]
-new_gp_sya_dates <- new_gp_sya_dates[new_gp_sya_dates <= last_res_date] # works because strings are in yyyy_mm format
+new_gp_sya_dates <- new_gp_sya_dates[new_gp_sya_dates <= last_res_date]
 
 sya_yyyy_mm <- new_gp_sya_dates
 sel_dates <- as.Date(paste0(new_gp_sya_dates, "_01"), "%Y_%m_%d")
@@ -32,8 +32,7 @@ gp_res <- lapply(gp_res_files, readRDS)%>%
   bind_rows()
 
 message("Calculating residence distributions for GP practices")
-# This bit is slow: interpolates the residence counts for the dates that the SYA data is given
-# Then calculates proportions for each LSOA
+
 prop_res <- calculate_gp_res_props(gp_res, sel_dates)
 
 if (file.exists(fpath$prop_res_lad)) {
