8ccdad05017dfb2d93988d3081c32bac280994d0
Ben Corr <bjcorr606@gmail.com>
2024-07-11 11:08:02 +0100

    stop clean_gp_res_lsoa from reading both 2011 and 2021 LSOA files

--- R/functions/clean_gp_res_lsoa.R
+++ R/functions/clean_gp_res_lsoa.R
@@ -8,8 +8,8 @@ clean_gp_res_lsoa <- function(dir_in,
   #currently not using the extract date variable in the data as it's somewhat inconsistently formatted
   #TODO add more robust parsing for the date variable and use it directly
 
-  fp_gp_lsoa_male <- list.files(dir_in, pattern = "gp-reg-pat-prac-lsoa-male", full.names = TRUE)
-  fp_gp_lsoa_female <- list.files(dir_in, pattern = "gp-reg-pat-prac-lsoa-female", full.names = TRUE)
+  fp_gp_lsoa_male <- list.files(dir_in, pattern = "gp-reg-pat-prac-lsoa-male.csv", full.names = TRUE)
+  fp_gp_lsoa_female <- list.files(dir_in, pattern = "gp-reg-pat-prac-lsoa-female.csv", full.names = TRUE)
 
   #this lookup is for dealing with inconsistencies in variable naming between files
   name_lookup <- c(`Number of Patients` = "NUMBER_OF_PATIENTS",
